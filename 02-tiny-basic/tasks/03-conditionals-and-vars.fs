f12953ee686bd73cfca19cfd418a8fbd659da023
Matěj Schrödl <matej.schrodl@gmail.com>
2025-10-23 15:15:31 +0200

    Tiny Basic -- 03 Done

--- 02-tiny-basic/tasks/03-conditionals-and-vars.fs
+++ 02-tiny-basic/tasks/03-conditionals-and-vars.fs
@@ -5,7 +5,6 @@ module TinyBASIC
 
 type Value =
   | StringValue of string
-  // NOTE: Added numerical and Boolean values
   | NumberValue of int
   | BoolValue of bool
 
@@ -21,15 +20,13 @@ type Command =
   | Print of Expression
   | Run 
   | Goto of int
-  // NOTE: Assign expression to a given variable and conditional that 
-  // runs a given Command only if the expression evaluates to 'BoolValue(true)'
   | Assign of string * Expression
   | If of Expression * Command
   | Empty
 
 type State = 
   { Program : list<int * Command> 
-    // TODO: Add variable context to the program state
+    Variables: Map<string, Value>
   }
 
 // ----------------------------------------------------------------------------
@@ -40,13 +37,17 @@ let printValue value : Unit =
   match value with
   | StringValue str-> 
     printfn "%s" str
+  | NumberValue n ->
+    printfn "%d" n
+  | BoolValue b ->
+    if b then printfn "true" else printfn "false"
 
-  | _ -> failwith "Not a valid value to print"
 
 let getLine state line =
   match state.Program |> List.tryFind( fun (next_line, _) -> next_line > line) with
   | Some (next_line, cmd) -> (next_line, cmd)
   | None -> (-1, Empty)
+
 let addLine state (line, cmd) = 
   let updatedProgram = 
     state.Program 
@@ -61,14 +62,40 @@ let addLine state (line, cmd) =
 // Evaluator
 // ----------------------------------------------------------------------------
 
-let rec evalExpression (expr: Expression) : Value =  
+let rec evalExpression (expr: Expression, state: State) : Value =  
   match expr with 
   | Const c -> c 
+  | Function (name, expr_list) ->
+    match name with 
+    | "+" -> 
+        let evaluatedArgs = List.map (fun expr -> evalExpression (expr, state)) expr_list
+        match evaluatedArgs with 
+        | [NumberValue lhs; NumberValue rhs] -> NumberValue(lhs + rhs)
+        | _ -> failwith "Invalid arguments for + function"
+    
+    | "-" -> 
+        let evaluatedArgs = List.map (fun expr -> evalExpression (expr, state)) expr_list
+        match evaluatedArgs with 
+        | [NumberValue lhs; NumberValue rhs] -> NumberValue(lhs - rhs)
+        | _ -> failwith "Invalid arguments for - function"
+
+    | "=" -> 
+        let evaluatedArgs = List.map (fun expr -> evalExpression (expr, state)) expr_list
+        match evaluatedArgs with
+        | [NumberValue lhs; NumberValue rhs] -> BoolValue(lhs = rhs)
+        | _ -> failwith "Invalid arguments for = function"
+    | _ -> failwith "Only binary options implemented"
+
+  | Variable v ->
+    match state.Variables |> Map.tryFind v with
+    | Some value -> 
+    value // Return the variable's value if it exists
+    | None -> failwithf "Variable '%s' not found" v // Handle missing variable
 
 let rec runCommand state (line, cmd) =
   match cmd with 
   | Print(expr) ->
-      let value = evalExpression expr
+      let value = evalExpression(expr,state)
       printValue value 
       runNextLine state line
   | Run ->
@@ -80,15 +107,21 @@ let rec runCommand state (line, cmd) =
       runCommand state (new_line, new_cmd)
   | Empty ->
       ignore
-  // TODO: Implement assignment and conditional. Assignment should run the
-  // next line after setting the variable value. 'If' is a bit trickier:
-  // * 'L1: IF TRUE THEN GOTO <L2>' will continue evaluating on line 'L2'
-  // * 'L1: IF FALSE THEN GOTO <L2>' will continue on line after 'L1'
-  // * 'L1: IF TRUE THEN PRINT "HI"' will print HI and continue on line after 'L1'
-  //
-  // HINT: If <e> evaluates to TRUE, you can call 'runCommand' recursively with
-  // the command in the 'THEN' branch and the current line as the line number.
-  | Assign _ | If _ -> failwith "not implemented"
+
+  | Assign (name, expr) -> 
+    let evaluated = evalExpression (expr, state)
+    let updated_context = state.Variables.Add(name, evaluated)
+    runNextLine { state with Variables = updated_context } line
+
+  | If (expr, cmd)  -> 
+    let evaluated: Value = evalExpression(expr, state)
+    match evaluated with
+    | BoolValue(true) -> 
+      runCommand state (-1, cmd)
+    | BoolValue(false) ->
+      runNextLine state line
+    | _ -> failwith "Not valid expression in an if statement"
+
 
 and runNextLine state line = 
   let (next_line, next_cmd) = getLine state line
@@ -109,8 +142,6 @@ let runInput (state : State) (line :option<int>, cmd : Command) : State =
   | Some ln ->
     addLine state (ln, cmd)
 
-
-
 let runInputs (state: State) (cmds: list<option<int> * Command>) : State =
   List.fold (fun accState cmd -> runInput accState cmd) state cmds
 
@@ -119,30 +150,19 @@ let runInputs (state: State) (cmds: list<option<int> * Command>) : State =
 // Test cases
 // ----------------------------------------------------------------------------
 
-let empty = { Program = [] } // TODO: Add empty variables to the initial state!
+let empty = { Program = []; Variables = Map.empty} // TODO: Add empty variables to the initial state!
 
-let helloOnce = 
-  [ Some 10, Print (Const (StringValue "HELLO WORLD\n")) 
-    Some 10, Print (Const (StringValue "HELLO NPRG077\n")) 
-    None, Run ]
+// let testVariables = 
+//   [ Some 10, Assign("S", Const(StringValue "HELLO WORLD\n")) 
+//     Some 20, Assign("I", Const(NumberValue 1))
+//     Some 30, Assign("B", Function("=", [Variable("I"); Const(NumberValue 1)]))
+//     Some 40, Print(Variable "S") 
+//     Some 50, Print(Variable "I") 
+//     Some 60, Print(Variable "B")
+//     None, Run ]
 
-let helloInf = 
-  [ Some 20, Goto 10
-    Some 10, Print (Const (StringValue "HELLO WORLD\n")) 
-    Some 10, Print (Const (StringValue "HELLO NPRG077\n")) 
-    None, Run ]
-
-let testVariables = 
-  [ Some 10, Assign("S", Const(StringValue "HELLO WORLD\n")) 
-    Some 20, Assign("I", Const(NumberValue 1))
-    Some 30, Assign("B", Function("=", [Variable("I"); Const(NumberValue 1)]))
-    Some 40, Print(Variable "S") 
-    Some 50, Print(Variable "I") 
-    Some 60, Print(Variable "B")
-    None, Run ]
-
-// NOTE: Simpler test program without 'If" (just variables and '=' function) 
-runInputs empty testVariables |> ignore
+// // // NOTE: Simpler test program without 'If" (just variables and '=' function) 
+// runInputs empty testVariables |> ignore
 
 let helloTen = 
   [ Some 10, Assign("I", Const(NumberValue 10))
@@ -153,5 +173,5 @@ let helloTen =
     Some 60, Print (Const(StringValue "")) 
     None, Run ]
 
-// NOTE: Prints hello world ten times using conditionals
+// // NOTE: Prints hello world ten times using conditionals
 runInputs empty helloTen |> ignore
